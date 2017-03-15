e3f140572f4a15512fa283011843bd9958c6bbac
Fakemonster <jgraythel@gmail.com>
2017-03-15 10:11:43 -0700

    doing dev on prod because i'm a bad dude

--- bot.js
+++ bot.js
@@ -16,26 +16,24 @@ bot.startRTM(function(err,bot,payload) {
   if (err) {
     throw new Error('Could not connect to Slack');
   }
-
-  // close the RTM for the sake of it in 5 seconds
-  /*setTimeout(function() {
-      bot.closeRTM();
-  }, 5000);*/
 });
 
 controller.hears('brewed', ['direct_mention', 'mention'], (bot, message) => {
   bot.reply(message, "That's great news! I'll tell everyone.");
-  //client.makeCoffee();
+  client.makeCoffee();
 });
 
 controller.hears('claimed', ['direct_mention', 'mention'], (bot, message) => {
   bot.reply(message, "You got it boss!");
-  //client.coffeeStatus();
+  client.coffeeStatus
+    .then( (status) => {
+      console.log(status);
+    });
 })
 
 controller.hears('status', ['direct_mention'], (bot, message) => {
   bot.reply(message, "I'll check!");
-  //client.coffeeStatus();
+  client.coffeeStatus();
 })
 
 http.createServer().listen(process.env.PORT || 3000).on('error', console.log);

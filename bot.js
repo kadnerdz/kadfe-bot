0dfdc4fd60fcc23e23560b48803743d15f9c4ecc
Fakemonster <jgraythel@gmail.com>
2017-03-19 11:19:12 -0700

    trying wss

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ var bot = controller.spawn({
   token: process.env.TOKEN
 });
 
-var ws = new WebSocket(`ws://${process.env.KADFE_HOST}/coffee`)
+var ws = new WebSocket(`wss://${process.env.KADFE_HOST}`)
 
 bot.startRTM(function(err,bot,payload) {
   if (err) {
